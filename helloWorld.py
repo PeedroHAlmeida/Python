ee1b1c23baa4fa14bf8f77f4d689e978b6d115f1
Pedro Henrique de Almeida <119445086+PeedroHAlmeida@users.noreply.github.com>
2024-07-20 23:41:17 -0300

    '-'

---
+++ helloWorld.py
@@ -0,0 +1,14 @@
+saida = "Hello World!"
+cont = 0
+
+for entrada in saida:
+    x = 0
+    while (cont > x):
+        print(end = " ")
+        x = x + 1
+    print(f"{entrada}")
+    print(f"{saida}\n")
+    cont = cont + 1
+
+
+
